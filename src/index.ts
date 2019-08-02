48ca3ef60a1b5c23bc89a41ecb52d7954b750e2c
Spencer Schoeben <spencer@netspencer.com>
2019-08-02 10:24:26 -0700

    Extracting logic to story class

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,31 @@
 import 'intersection-observer'
-import lottie from 'lottie-web'
-import scrollama from 'scrollama'
-import Postcard from './postcard'
-
-// init when window is ready
+import Story from './story'
 ;(function(window) {
-  const loading = lottie.loadAnimation({
-    container: document.getElementById('loading-animation'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'https://assets4.lottiefiles.com/datafiles/rFr1le9E8lhiQjf/data.json'
-  })
-
-  const postcard = new Postcard(document.getElementById('postcard'), window)
-
-  setTimeout(() => {
-    document.getElementById('postcard').style.visibility = 'visible'
-    loading.destroy()
-    document.querySelector('body').classList.remove('overflow-hidden')
-    // setTimeout(() => {
-    //   window.scrollTo({ top: window.innerHeight })
-    // }, 500)
-  }, 1000)
-
-  // // instantiate the scrollama
-  // const scroller = scrollama()
-
-  // // setup the instance, pass callback functions
-  // scroller
-  //   .setup({
-  //     step: '.step'
-  //   })
-  //   .onStepEnter(response => {
-  //     console.log(response)
-  //     // { element, index, direction }
-  //   })
-  //   .onStepExit(response => {
-  //     // { element, index, direction }
-  //   })
-
-  // // setup resize event
-  // window.addEventListener('resize', scroller.resize)
+  new Story(window, document)
 })(window)
+
+// const postcard = new Postcard(document.getElementById('postcard'), window)
+// setTimeout(() => {
+//   document.getElementById('postcard').style.visibility = 'visible'
+//   loading.destroy()
+//   document.querySelector('body').classList.remove('overflow-hidden')
+//   // setTimeout(() => {
+//   //   window.scrollTo({ top: window.innerHeight })
+//   // }, 500
+// }, 700)
+// // // instantiate the scrollama
+// const scroller = scrollama()
+// scroller
+//   .setup({
+//     step: '.step'
+//   })
+//   .onStepEnter(({ index, direction }) => {
+//     console.log('enter', index, direction)
+//     // console.log(response)
+//     // { element, index, direction }
+//   })
+//   .onStepExit(({ index, direction }) => {
+//     console.log('exit', index, direction)
+//     // { element, index, direction }
+//   })
+// window.addEventListener('resize', scroller.resize)
---
+++ src/story.ts
@@ -0,0 +1,47 @@
+import lottie, { AnimationItem } from 'lottie-web'
+import Postcard from './postcard'
+
+export default class Story {
+  window: Window
+  document: Document
+
+  loadingAnimation: AnimationItem
+  postcard: Postcard
+
+  constructor(window: Window, document: Document) {
+    this.window = window
+    this.document = document
+
+    this.setup()
+    this.start()
+  }
+
+  setup = () => {
+    // - setup loading
+    this.loadingAnimation = lottie.loadAnimation({
+      container: this.document.getElementById('loading-animation'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path:
+        'https://assets4.lottiefiles.com/datafiles/rFr1le9E8lhiQjf/data.json'
+    })
+
+    // - setup postcard
+    this.postcard = new Postcard(
+      this.document.getElementById('postcard'),
+      this.window
+    )
+  }
+
+  start = () => {
+    setTimeout(() => {
+      this.document.getElementById('postcard').style.visibility = 'visible'
+      this.loadingAnimation.destroy()
+      this.document.querySelector('body').classList.remove('overflow-hidden')
+      // setTimeout(() => {
+      //   window.scrollTo({ top: window.innerHeight })
+      // }, 500
+    }, 700)
+  }
+}
