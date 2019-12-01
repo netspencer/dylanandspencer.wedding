6163593bf0eea3fb7e56a928f52b8e92d9343ccb
Spencer Schoeben <spencer@netspencer.com>
2019-12-01 15:57:10 -0800

    Working on /story with mdx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -24,7 +24,7 @@ const Layout: FunctionComponent<Props> = ({
         rel="stylesheet"
       />
     </Head>
-    <main className="relative container">
+    <main className="relative">
       <Navigation />
       {children}
     </main>
