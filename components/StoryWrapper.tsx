59ea8d327aedd56b32c0a501894bd168deb286f4
Spencer Schoeben <spencer@netspencer.com>
2019-12-01 17:50:27 -0800

    Recreate my story section in tailwind css

--- components/StoryWrapper.tsx
+++ components/StoryWrapper.tsx
@@ -1,12 +1,17 @@
 import React, { FunctionComponent } from 'react'
 import { MDXProvider, Components } from '@mdx-js/react'
+import classNames from 'classnames'
 
 const components: Components = {
-  p: ({ children }) => <p className="lh-copy f4 pv2">{children}</p>
+  p: ({ children }) => (
+    <p className={classNames('leading-normal', 'text-xl', 'my-8')}>
+      {children}
+    </p>
+  )
 }
 
 const StoryWrapper: FunctionComponent = ({ children }) => (
-  <article className="mv7 libre mw7 center">
+  <article className={classNames('libre', 'max-w-3xl', 'mx-auto')}>
     <MDXProvider components={components}>{children}</MDXProvider>
   </article>
 )
--- pages/story.tsx
+++ pages/story.tsx
@@ -7,21 +7,9 @@ import StoryWrapper from '../components/StoryWrapper'
 const StoryPage: NextPage = () => {
   return (
     <Layout>
-      {/* <StoryWrapper>
+      <StoryWrapper>
         <StoryDocument />
-      </StoryWrapper> */}
-
-      {/* <section className="step dt w-100 relative">
-                <div className="dtc v-mid wedding-gray ph3 ph4-l">
-                  <div className="tj pv5 pv6-ns ph5-ns lh-copy w-60-ns fl-ns f3"></div>
-                </div>
-              </section> */}
-
-      {/* <section className="vh-100 dt w-100 relative no-selection">
-        <div className="dtc v-mid tc">
-          <div className="crawl-container libre pv5 pa6-ns top-0"></div>
-        </div>
-      </section> */}
+      </StoryWrapper>
     </Layout>
   )
 }
