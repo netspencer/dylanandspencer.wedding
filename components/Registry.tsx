9b8857c9b07cd4e7d66d54d2c6bfd59df63b270b
Spencer Schoeben <spencer@netspencer.com>
2020-01-10 22:21:21 -0800

    Add loading for registry while iframe is loading

---
+++ components/Registry.tsx
@@ -0,0 +1,25 @@
+import React, { FunctionComponent, useState } from 'react'
+import Iframe from 'iframe-resizer-react'
+import classNames from 'classnames'
+import Loading from './Loading'
+
+const Registry: FunctionComponent = () => {
+  const [didLoad, setDidLoad] = useState(false)
+
+  return (
+    <div>
+      {!didLoad && <Loading />}
+      <Iframe
+        style={{ transition: 'opacity 1s' }}
+        onInit={() => {
+          setDidLoad(true)
+        }}
+        className={classNames('w-full', { 'opacity-0': !didLoad })}
+        bodyBackground="rgba(0,0,0,0)"
+        src="https://widget.zola.com/v1/widget/registry/dylanandspencer/html"
+      />
+    </div>
+  )
+}
+
+export default Registry
--- pages/registry.tsx
+++ pages/registry.tsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import { NextPage } from 'next'
 import Layout from '../components/Layout'
-import Iframe from 'iframe-resizer-react'
+import Registry from '../components/Registry'
 
 const RegistryPage: NextPage = () => {
   return (
     <Layout>
       <section className="vh-100 dt w-100 relative no-selection">
         <div className="dtc v-mid tc">
-          <Iframe
-            className="w-full"
-            bodyBackground="rgba(0,0,0,0)"
-            src="https://widget.zola.com/v1/widget/registry/dylanandspencer/html"
-          />
+          <Registry />
         </div>
       </section>
     </Layout>
