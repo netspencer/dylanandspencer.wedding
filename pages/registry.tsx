416eac1d5d338c7bdcc5448ae3e21a9bd4251a3b
Spencer Schoeben <spencer@netspencer.com>
2020-01-10 17:04:13 -0800

    Embed registry into site

--- pages/registry.tsx
+++ pages/registry.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { NextPage } from 'next'
 import Layout from '../components/Layout'
+import Iframe from 'iframe-resizer-react'
 
 const RegistryPage: NextPage = () => {
   return (
     <Layout>
       <section className="vh-100 dt w-100 relative no-selection">
         <div className="dtc v-mid tc">
-          <h2>Registry</h2>
+          <Iframe
+            className="w-full"
+            bodyBackground="rgba(0,0,0,0)"
+            src="https://widget.zola.com/v1/widget/registry/dylanandspencer/html"
+          />
         </div>
       </section>
     </Layout>
