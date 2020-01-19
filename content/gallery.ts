bd30c3f9ee968ef88bbe38a044a4f725beee529d
Spencer Schoeben <spencer@netspencer.com>
2020-01-19 15:42:28 -0800

    Adding first pass at image gallery

--- content/gallery.ts
+++ content/gallery.ts
@@ -1,46 +1,201 @@
 export const photos = [
   {
-    src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599',
+    src: '/gallery/1.jpeg',
     width: 4,
     height: 3
   },
   {
-    src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799',
-    width: 1,
-    height: 1
-  },
-  {
-    src: 'https://source.unsplash.com/qDkso9nvCg0/600x799',
+    src: '/gallery/2.jpeg',
     width: 3,
     height: 4
   },
   {
-    src: 'https://source.unsplash.com/iecJiKe_RNg/600x799',
+    src: '/gallery/4.jpeg',
     width: 3,
     height: 4
   },
   {
-    src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799',
-    width: 3,
-    height: 4
-  },
-  {
-    src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599',
+    src: '/gallery/5.jpeg',
     width: 4,
     height: 3
   },
   {
-    src: 'https://source.unsplash.com/zh7GEuORbUw/600x799',
+    src: '/gallery/6.jpeg',
     width: 3,
     height: 4
   },
   {
-    src: 'https://source.unsplash.com/PpOHJezOalU/800x599',
+    src: '/gallery/7.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/8.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/9.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/10.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/11.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/12.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/13.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/14.jpeg',
     width: 4,
     height: 3
   },
   {
-    src: 'https://source.unsplash.com/I1ASdgphUH4/800x599',
+    src: '/gallery/15.jpeg',
+    width: 4,
+    height: 3
+  },
+  {
+    src: '/gallery/16.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/17.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/18.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/19.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/20.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/22.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/23.jpeg',
+    width: 4,
+    height: 3
+  },
+  {
+    src: '/gallery/24.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/25.jpeg',
+    width: 4,
+    height: 3
+  },
+  {
+    src: '/gallery/26.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/27.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/28.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/29.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/30.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/31.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/32.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/33.jpeg',
+    width: 4,
+    height: 3
+  },
+  {
+    src: '/gallery/34.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/35.jpeg',
+    width: 4,
+    height: 3
+  },
+  {
+    src: '/gallery/36.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/37.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/38.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/39.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/40.jpeg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/gallery/41.jpeg',
+    width: 4,
+    height: 3
+  },
+  {
+    src: '/gallery/42.jpeg',
     width: 4,
     height: 3
   }
