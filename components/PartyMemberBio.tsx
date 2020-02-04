e4073d4b2a9425cee777c3df412f454a8125f5c7
Spencer Schoeben <spencer@netspencer.com>
2020-02-03 22:03:05 -0800

    WIP - wedding party

---
+++ components/PartyMemberBio.tsx
@@ -0,0 +1,33 @@
+import React, { FunctionComponent } from 'react'
+import classNames from 'classnames'
+
+interface Props {
+  name: string
+  image?: string
+  bio: string
+}
+
+const PartyMemberBio: FunctionComponent<Props> = ({ name, image, bio }) => (
+  <article className={classNames('w-full', 'md:w-1/2', 'lg:w-1/4')}>
+    <div className={classNames('p-4')}>
+      <h1 className={classNames('libre', 'text-2xl', 'font-bold')}>{name}</h1>
+      <div className={classNames('photo', 'relative', 'bg-green-200', 'my-4')}>
+        {image && (
+          <img
+            className={classNames('absolute', 'top-0', 'left-0', 'w-full')}
+            src={image}
+            alt={name}
+          />
+        )}
+      </div>
+      <p className={classNames('libre')}>{bio}</p>
+    </div>
+    <style jsx>{`
+      .photo {
+        padding-top: 125%;
+      }
+    `}</style>
+  </article>
+)
+
+export default PartyMemberBio
