f5e53c6b2681ac20de5205d0415f7d7eb83aedfa
Spencer Schoeben <spencer@netspencer.com>
2020-02-03 19:51:14 -0800

    Fix photo dimensions on Memories page

--- content/gallery.ts
+++ content/gallery.ts
@@ -1,202 +1,182 @@
 export const photos = [
   {
     src: '/gallery/1.jpeg',
-    width: 4,
-    height: 3
+    width: 1280,
+    height: 962
   },
   {
     src: '/gallery/2.jpeg',
-    width: 3,
-    height: 4
-  },
-  {
-    src: '/gallery/4.jpeg',
-    width: 3,
-    height: 4
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/5.jpeg',
-    width: 4,
-    height: 3
+    width: 1280,
+    height: 962
   },
   {
     src: '/gallery/6.jpeg',
-    width: 3,
-    height: 4
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/7.jpeg',
-    width: 3,
-    height: 4
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/8.jpeg',
-    width: 3,
-    height: 4
+    width: 759,
+    height: 1280
   },
   {
     src: '/gallery/9.jpeg',
-    width: 3,
-    height: 4
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/10.jpeg',
-    width: 3,
-    height: 4
+    width: 854,
+    height: 1280
   },
   {
     src: '/gallery/11.jpeg',
-    width: 3,
-    height: 4
-  },
-  {
-    src: '/gallery/12.jpeg',
-    width: 3,
-    height: 4
-  },
-  {
-    src: '/gallery/13.jpeg',
-    width: 3,
-    height: 4
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/14.jpeg',
-    width: 4,
-    height: 3
+    width: 1280,
+    height: 960
   },
   {
     src: '/gallery/15.jpeg',
-    width: 4,
-    height: 3
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/16.jpeg',
-    width: 3,
-    height: 4
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/17.jpeg',
-    width: 3,
-    height: 4
+    width: 872,
+    height: 1280
   },
   {
     src: '/gallery/18.jpeg',
-    width: 3,
-    height: 4
+    width: 1028,
+    height: 1280
   },
   {
     src: '/gallery/19.jpeg',
-    width: 3,
-    height: 4
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/20.jpeg',
-    width: 3,
-    height: 4
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/22.jpeg',
-    width: 3,
-    height: 4
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/23.jpeg',
-    width: 4,
-    height: 3
+    width: 1280,
+    height: 960
   },
   {
     src: '/gallery/24.jpeg',
-    width: 3,
-    height: 4
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/25.jpeg',
-    width: 4,
-    height: 3
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/26.jpeg',
-    width: 3,
-    height: 4
+    width: 962,
+    height: 1280
   },
   {
     src: '/gallery/27.jpeg',
-    width: 3,
-    height: 4
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/28.jpeg',
-    width: 3,
-    height: 4
+    width: 720,
+    height: 1280
   },
   {
     src: '/gallery/29.jpeg',
-    width: 3,
-    height: 4
-  },
-  {
-    src: '/gallery/30.jpeg',
-    width: 3,
-    height: 4
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/31.jpeg',
-    width: 3,
-    height: 4
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/32.jpeg',
-    width: 3,
-    height: 4
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/33.jpeg',
-    width: 4,
-    height: 3
+    width: 640,
+    height: 480
   },
   {
     src: '/gallery/34.jpeg',
-    width: 3,
-    height: 4
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/35.jpeg',
-    width: 4,
-    height: 3
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/36.jpeg',
-    width: 3,
-    height: 4
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/37.jpeg',
-    width: 3,
-    height: 4
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/38.jpeg',
-    width: 3,
-    height: 4
+    width: 962,
+    height: 1280
   },
   {
     src: '/gallery/39.jpeg',
-    width: 3,
-    height: 4
+    width: 960,
+    height: 1280
   },
   {
     src: '/gallery/40.jpeg',
-    width: 3,
-    height: 4
+    width: 720,
+    height: 1280
   },
   {
     src: '/gallery/41.jpeg',
-    width: 4,
-    height: 3
+    width: 1242,
+    height: 819
   },
   {
     src: '/gallery/42.jpeg',
-    width: 4,
-    height: 3
+    width: 1280,
+    height: 960
   }
 ]
