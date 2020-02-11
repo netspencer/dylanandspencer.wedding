c0d5fddd4c78abf94e8db6888756d0a41c32a92e
Spencer Schoeben <spencer@netspencer.com>
2020-02-10 17:08:42 -0800

    Fix sizing of event map

--- components/EventMap.tsx
+++ components/EventMap.tsx
@@ -8,17 +8,39 @@ const Map = ReactMapboxGl({
 })
 
 const EventMap: FunctionComponent = () => (
-  <div className={classNames('p-8')}>
-    <Map
-      style="mapbox://styles/mapbox/streets-v9"
-      containerStyle={{ width: '100%', height: '600px' }}
-      center={[-118.655891, 34.286331]}
-      zoom={[10]}
-    >
-      <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
-        <Feature coordinates={[-118.655891, 34.286331]} />
-      </Layer>
-    </Map>
+  <div className={classNames('sm:p-8')}>
+    <article className={classNames('relative')}>
+      <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        containerStyle={{ position: 'absolute' }}
+        className={classNames(
+          'w-full',
+          'absolute',
+          'left-0',
+          'right-0',
+          'top-0',
+          'bottom-0',
+          'h-full',
+          'h-64'
+        )}
+        center={[-118.655891, 34.286331]}
+        zoom={[10]}
+      >
+        <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+          <Feature coordinates={[-118.655891, 34.286331]} />
+        </Layer>
+      </Map>
+    </article>
+    <style jsx>{`
+      article {
+        padding-top: 100%;
+      }
+      @media (min-width: 640px) {
+        article {
+          padding-top: 66%;
+        }
+      }
+    `}</style>
   </div>
 )
 
