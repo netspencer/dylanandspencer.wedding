9d318cdd934a3d8d6897c77e40a4249726863eb5
Spencer Schoeben <spencer@netspencer.com>
2020-02-17 21:10:51 -0800

    Add who, what, when info to 'the big day'

--- components/EventMap.tsx
+++ components/EventMap.tsx
@@ -8,29 +8,27 @@ const Map = ReactMapboxGl({
 })
 
 const EventMap: FunctionComponent = () => (
-  <div className={classNames('sm:p-8')}>
-    <article className={classNames('relative')}>
-      <Map
-        style="mapbox://styles/mapbox/streets-v9"
-        containerStyle={{ position: 'absolute' }}
-        className={classNames(
-          'w-full',
-          'absolute',
-          'left-0',
-          'right-0',
-          'top-0',
-          'bottom-0',
-          'h-full',
-          'h-64'
-        )}
-        center={[-118.655891, 34.286331]}
-        zoom={[10]}
-      >
-        <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
-          <Feature coordinates={[-118.655891, 34.286331]} />
-        </Layer>
-      </Map>
-    </article>
+  <article className={classNames('relative')}>
+    <Map
+      style="mapbox://styles/mapbox/streets-v9"
+      containerStyle={{ position: 'absolute' }}
+      className={classNames(
+        'w-full',
+        'absolute',
+        'left-0',
+        'right-0',
+        'top-0',
+        'bottom-0',
+        'h-full',
+        'h-64'
+      )}
+      center={[-118.655891, 34.286331]}
+      zoom={[10]}
+    >
+      <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+        <Feature coordinates={[-118.655891, 34.286331]} />
+      </Layer>
+    </Map>
     <style jsx>{`
       article {
         padding-top: 100%;
@@ -41,7 +39,7 @@ const EventMap: FunctionComponent = () => (
         }
       }
     `}</style>
-  </div>
+  </article>
 )
 
 export default EventMap
--- components/WeddingDetails.tsx
+++ components/WeddingDetails.tsx
@@ -5,10 +5,80 @@ import dynamic from 'next/dynamic'
 const EventMap = dynamic(() => import('./EventMap'), { ssr: false })
 
 const WeddingDetails: FunctionComponent = () => (
-  <section className={classNames('container', 'mx-auto')}>
-    <EventMap />
-    <h1>The Big Day</h1>
-  </section>
+  <>
+    <section
+      className={classNames(
+        'container',
+        'mx-auto',
+        'px-2',
+        'py-4',
+        'sm:px-8',
+        'text-center',
+        'libre'
+      )}
+    >
+      <h1 className={classNames('lg:text-2xl', 'md:text-xl')}>The Wedding</h1>
+      <h1 className={classNames('lg:text-3xl', 'md:text-2xl', 'text-lg')}>
+        <span className={classNames('sm:inline', 'block')}>
+          Dylan Michelle Newman{' '}
+        </span>
+        <span className={classNames('sm:inline', 'block')}>
+          &amp; Spencer Davis Schoeben
+        </span>
+      </h1>
+      <h1
+        className={classNames(
+          'lg:text-4xl',
+          'md:text-3xl',
+          'text-xl',
+          'font-bold'
+        )}
+      >
+        <span className={classNames('sm:inline', 'block')}>
+          Sunday, May 24th, 2020{' '}
+        </span>
+        <span className={classNames('sm:inline', 'block')}>at 5:30pm</span>
+      </h1>
+    </section>
+    <section
+      className={classNames(
+        'container',
+        'mx-auto',
+        'py-4',
+        'sm:px-8',
+        'flex',
+        'justify-center',
+        'items-center',
+        'flex-wrap',
+        'md:flex-no-wrap'
+      )}
+    >
+      <div className={classNames('md:w-1/2', 'lg:w-2/3', 'w-full', 'md:mr-8')}>
+        <EventMap />
+      </div>
+      <div
+        className={classNames(
+          'md:w-1/2',
+          'lg:w-1/3',
+          'w-full',
+          'libre',
+          'text-xl',
+          'xl:text-2xl',
+          'text-center',
+          'md:text-left',
+          'mt-8',
+          'md:mt-0'
+        )}
+      >
+        <h1 className={classNames('font-bold')}>Hummingbird Nest Ranch</h1>
+        <h2>
+          2940 Kuehner Drive
+          <br />
+          Santa Susana, CA 93063
+        </h2>
+      </div>
+    </section>
+  </>
 )
 
 export default WeddingDetails
