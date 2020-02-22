7bfff86f8703ce105984c256c1a6c681385c653a
Spencer Schoeben <spencer@netspencer.com>
2020-02-22 08:31:13 -0800

    Update grammar errors in our story

---
+++ components/Countdown.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react'
+import classNames from 'classnames'
+
+const Countdown: FunctionComponent = () => (
+  <section className={classNames('p-8')}>88 days until wedding</section>
+)
+
+export default Countdown
