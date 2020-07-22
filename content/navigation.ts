07946720624a5bdd580beb8cb6c03878a451e57b
Spencer Schoeben <spencer@netspencer.com>
2020-07-22 10:57:07 -0700

    feat: add COVID to navigation

--- content/navigation.ts
+++ content/navigation.ts
@@ -1,13 +1,14 @@
 const navigation = {
   pages: [
+    { title: 'COVID Update', href: '/covid' },
     { title: 'Our Story', href: '/story' },
     { title: 'Wedding Party', href: '/party' },
     { title: 'Memories', href: '/memories' },
     { title: 'The Big Day', href: '/wedding' },
     { title: 'Registry', href: '/registry' },
     { title: 'Getting There', href: '/travel' },
-    { title: 'Things To Do in LA', href: '/los-angeles' }
-  ]
+    { title: 'Things To Do in LA', href: '/los-angeles' },
+  ],
 }
 
 export default navigation
